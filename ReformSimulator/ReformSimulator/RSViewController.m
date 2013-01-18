b4188744b269d452059096fff942709aa07442f2
yamadakei <yamadakei1979@gmail.com>
2013-01-18 15:20:33 +0900

    ReformSimulatorプロジェクト作成。Shared Applicatio実装。plist作成。

---
+++ ReformSimulator/ReformSimulator/RSViewController.m
@@ -0,0 +1,38 @@
+//
+//  RSViewController.m
+//  ReformSimulator
+//
+//  Created by 山田 慶 on 2013/01/18.
+//  Copyright (c) 2013年 山田 慶. All rights reserved.
+//
+
+#import "RSViewController.h"
+
+@interface RSViewController ()
+
+@end
+
+@implementation RSViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
